e705ecff4d8a1c2e054bfdb46b8639ec99c43d6c
EDNEI DOS SANTOS NASCIMENTO JUNIOR <09428446530@unijorge.com.br>
2026-08-25 20:52:22 -0300

    commit part3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Pessoa } from './pessoa/pessoa';
+import { Home } from './home/home';
+export const routes: Routes = [
+    {path:  'pessoa', component: Pessoa},
+    {path: 'Home',   component: Home  },
+];
